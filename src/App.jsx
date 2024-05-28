847c314e84a35d3d17520db0e471a20109965896
SSS-Monocept <shouryasinghsengar@monocept.com>
2024-05-28 16:35:03 +0530

    Better Todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,62 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
 import "./App.css";
 
 function App() {
-  return <>Hello Vercel.com !!</>;
+  const [todos, setTodos] = useState([
+    {
+      title: "Go to Gym",
+      description: "Go to the gym from 7-9",
+      completed: false,
+    },
+    {
+      title: "Study DSA",
+      description: "Study DSA from 9-10",
+      completed: true,
+    },
+  ]);
+
+  return (
+    <div className="app-container">
+      <h1>Welcome to Your Todo List</h1>
+      <button
+        onClick={() => {
+          setTodos([
+            ...todos,
+            {
+              title: "New todo",
+              description: "Description of the new todo...",
+            },
+          ]);
+        }}
+      >
+        Add Todo
+      </button>
+      {todos.map(function (todo, index) {
+        return (
+          <Todo
+            key={index}
+            title={todo.title}
+            description={todo.description}
+          ></Todo>
+        );
+      })}
+    </div>
+  );
 }
+
+function Todo({ title, description }) {
+  return (
+    <div className="todo-item">
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+Todo.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 export default App;
